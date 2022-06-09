f9f06350ec460f27215fe2c6a526a324745563a0
sujon15-2813 <sujon15-2813@diu.edu.bd>
2022-06-09 21:50:55 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+
+import 'package:bmi_cal/constants/app_constants.dart';
+import 'package:bmi_cal/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "BMI Calculator",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeSreen(),
+      
+    );
+  }
+}
